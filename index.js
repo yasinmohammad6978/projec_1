cbb9ad48b5daf1b4b60fea38a69468094f329107
Mohammad Yasin <99276678+yasinmohammad6978@users.noreply.github.com>
2022-03-25 03:02:46 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+//creatin funtion
+
+function sqaure(){
+    var num = 5;
+var result= num  * 2;
+document.write("result" + result + "<br>");
+}
+sqaure();
+
+
+var num = 5;
+var result= num  * num;
+document.write("result" + result + "<br>");
+
+var num = 6;
+var result= num  + num;
+document.write("result" + result + "<br>");
+
+var num = 9;
+var result= num  - num;
+document.write("result" + result + "<br>");
+
+//calling funtion
+
+function sqaure(num){
+
+var result= num  * 2;
+document.write("the result" + result + "<br>");
+}
+sqaure(55);
+// creat funtion
+
+
+function sqaure(num1,num2){
+    var result= num1 * num2;
+document.write("Result="+ result+ "<br>")
+
+}
+sqaure(5,6);
+
+//add funtion
+
+function add(x,y){
+    var result= x+y;
+    document.write("sum"+result+ "<br>");
+}
+
+add(10,20)
+add(3,5);
+//sub
+function sub(x,y){
+    var result= x-y;
+    document.write("sum"+result+ "<br>");
+}
+
+sub(10,20)
+sub(3,5);
+
+//
